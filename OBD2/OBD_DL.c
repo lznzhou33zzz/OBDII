ed0e9ddabc950995a5fed0dd22e70d9b64f1332b
zezhongzhao <lznzhou33zzz@gmail.com>
2013-11-11 20:09:46 +0800

    update initialization sequence

--- OBD2/OBD_DL.c
+++ OBD2/OBD_DL.c
@@ -7,55 +7,116 @@
 #include "Common.h"
 #include "OBD_DL.h"
 
+typedef enum{
+	StateA,
+	StateB,
+	StateC,
+	StateD,
+	StateE,
+	StateF
+}IniReturnType;
 
-enum
-{
-	ID11_ONLY,
-	ID29_ONLY,
-	ID11_ID29,
-	NO_SUPPROT
-}SupportType;
+
+typedef enum{
+	setBautrate,
+	transmit,
+	waitResponse,
+	transmitDone,
+	errorDetaction,
+	judgmentCAN11InitEnd,
+	resetBautrateAndRetry,
+	transmitError,
+}CAN11InitReqState_Type;
+
+
+
+IniReturnType init_state;
 char Init_sequence(void){
-	char IsSupport = NOT_SUPPORT;
-	char IsID11_recieved,IsID29_received;
-	SupportType Support = NO_SUPPROT;
-
-	/*--------1. 11bit request --------*/
-
-	/*--------2. wait 11bit response --------*/
-
-	/*--------3. 29bit request --------*/
-
-	/*--------4. wait 29bit response --------*/
-
-	/*--------5. Judgment --------*/
-	 if(IsID29_received == RECEIVED && IsID11_recieved == RECEIVED)
-	{
-		Support = ID11_ID29;
-		IsSupport = SUPPORTED;
-		return	IsSupport;
-	}
-	else if(IsID29_received == NOT_RECEIVED && IsID11_recieved == RECEIVED)
-	{
-		Support = ID11_ONLY;
-		IsSupport = SUPPORTED;
-		return	IsSupport;
-	}
-	else if(IsID29_received == RECEIVED && IsID11_recieved == NOT_RECEIVED)
-	{
-		Support = ID29_ONLY;
-		IsSupport = SUPPORTED;
-		return	IsSupport;
-	}
-	else
-	{
-		return IsSupport;
-	}
-
+	do{
+		/*--------1. 11bit request --------------*/
+		if(init_state == StateA)
+		{init_state = CAN11IdentifierReq();}
+		/*--------2. wait 11bit response --------*/
+		if(init_state == StateB)
+		{init_state = CAN11IdentifierResq();}
+		/*--------3. 29bit request --------------*/
+		if(init_state == StateC)
+		{init_state = CAN29IdentifierReq();}
+		/*--------4. wait 29bit response --------*/
+		if(init_state == StateD)
+		{init_state = CAN11IdentifierResq();}
+	}while(init_state != StateE || init_state != StateF);
 }
 
 
+IniReturnType CAN11IdentifierReq(void)
+{
+	CAN11InitReqState_Type state;
+	state = setBautrate;
+	do{
+		/*----setup baudrate --------------------------*/
+		if(state == setBautrate)
+		{
+			;
+		}
+		/*----Transmit functional request CAN identifier
+			  service $01,PID`s supported request message
+			  as specified in ISO 15031-5--------------*/
+		if(state == transmit)
+		{
+			;
+		}
+		/**/
+		if(state == waitResponse)
+		{
+			;
+		}
+		/**/
+		if(state == errorDetaction)
+		{
+			;
+		}
+		/**/
+		if(state == judgmentCAN11InitEnd)
+		{
+			;
+		}
+		/*----Disconnect from CAN bus reset CAN
+		 	  controller------------------------------*/
+		if(state == resetBautrateAndRetry)
+		{
+			;
+		}
+	}while(state == transmitDone || state == transmitError);
+	if(state == transmitDone){
+		return StateB;
+	}
+	else{
+		return StateF;
+	}
+}
+
+IniReturnType CAN11IdentifierResp(void)
+{
 
 
 
+	return StateE;
+}
+
+IniReturnType CAN29IdentifierReq(void)
+{
+
+
+
+	return StateE;
+}
+
+IniReturnType CAN29IdentifierResp(void)
+{
+
+
+
+	return StateE;
+}
 
